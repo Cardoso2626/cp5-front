39dbec3c86ab065f77ca0ecd0d6a98b07de03f47
TamaraBoni <tamara_boni@hotmail.com>
2024-10-01 23:05:01 -0300

    Conteudo segunda pag ideia

--- cp5-front/src/app/segundaIdeia/page.tsx
+++ cp5-front/src/app/segundaIdeia/page.tsx
@@ -1,8 +1,8 @@
 export default function segundaIdeia(){
     return(
         <div>
-            <h1></h1>
-             <p></p>
+            <h1>Reinterpretação da Mitologia</h1>
+             <p>Ele propõe que muitos mitos e histórias antigas, como as de deuses e catástrofes, não são meras fábulas, mas relatos de eventos reais que ocorreram em nossa história. Velikovsky argumenta que esses mitos são reflexões de experiências coletivas da humanidade diante de desastres naturais, e que eles podem ser usados para entender a história antiga. </p>
         </div>
     )
 }
