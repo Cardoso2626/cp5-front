89517b9fe5b58c98da18557aead985337eccc545
TamaraBoni <tamara_boni@hotmail.com>
2024-10-01 23:05:58 -0300

    Conteudo pagina ideia 3

--- cp5-front/src/app/terceiraIdeia/page.tsx
+++ cp5-front/src/app/terceiraIdeia/page.tsx
@@ -1,7 +1,8 @@
 export default function terceiraIdeia(){
     return(
         <div>
-            <h1>terceira ideia</h1>
+            <h1>Sincronismo das Civilizações</h1>
+            <p>Velikovsky defende que várias civilizações, como a egípcia, a hebraica e a mesopotâmica, tiveram interações mais diretas do que se supunha. Ele argumenta que eventos cósmicos e catástrofes afetaram simultaneamente diversas culturas, resultando em uma história entrelaçada que deve ser compreendida de forma mais holística.</p>
         </div>
     )
 }
